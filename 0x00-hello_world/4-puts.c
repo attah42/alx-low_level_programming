04aadcb142cc776c402123df0920b821fde0edce
attah42 <you@example.comgit config --global user.name Your>
2022-10-17 09:21:16 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-/* this program prints "Programming is like building a multilingual puzzle"*/
+/* this program prints "Programming is like building a multilingual puzzle
+ *
+ * * Return: 0
+ *
+ */
 
 int main(void)
 
 {
-puts("\"Programming is like building a multilingual puzzle""\n");
-return (0);
+
+		puts("\"Programming is like building a multilingual puzzle""\n")
+
+			return (0);
+
 }
