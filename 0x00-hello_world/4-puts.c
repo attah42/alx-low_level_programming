c648d32968f0619903aceb455c872b4e867b1089
attah42 <you@example.comgit config --global user.name Your>
2022-10-17 09:04:20 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,19 +2,7 @@
 
 
 
-/**
- *
- * * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+/* this program prints "Programming is like building a multilingual puzzle"*/
 
 int main(void)
 
