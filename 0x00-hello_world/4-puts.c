dd09e49bfb7a2477452dcc1aab28ffab0b45cfe4
attah42 <you@example.comgit config --global user.name Your>
2022-10-17 09:42:58 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main(void)
+
+
+	/**
+	 *
+	 * main - print the string in the put function
+	 *
+	 * Description: using the main function
+	 * this program prints "Programming is like building a multilingual puzzle
+	 * Return0
+	 */
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
