e26f585741ac22b34ef90dbc4705defa445ff98a
attah42 <you@example.comgit config --global user.name Your>
2022-10-19 06:44:07 -0700

    mew

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-/**
- *
+/** 
  * main - print the string in the put function
  *
  * Description: using the main function
- *
  * this program prints "Programming is like building a multilingual puzzle
- *
  * Return: 0
- *
  */
 
 int main(void)
@@ -30,9 +26,12 @@ int main(void)
 							printf("Size of a char: %ld byte(s)\n", sizeof(c));
 
 								printf("Size of an int: %ld byte(s)\n", sizeof(i));
-								printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-							        printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-									printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+									printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+
+										printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+
+											printf("Size of a float: %ld byte(s)\n", sizeof(f));
 
 												return (0);
 
