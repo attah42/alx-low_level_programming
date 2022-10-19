8ea710cd717ed92cd622d2a324718372e3379f63
attah42 <you@example.comgit config --global user.name Your>
2022-10-19 07:11:17 -0700

    new

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+		cout("\ "and that piece of art is useful - Dora Korpar, 2015-10-19"\n");
+
+			return (0);
