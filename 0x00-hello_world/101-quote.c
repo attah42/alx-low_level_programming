eb8280540d3be3427d9ae536ebc4ca9e92506698
attah42 <you@example.comgit config --global user.name Your>
2022-10-19 07:47:18 -0700

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+*main - Prints out the last part of a quote in the standard error.
+* Return: 1 if success.
 */
-
-int main()
-
+int main(void)
+	
 {
 
-		cout("\ "and that piece of art is useful - Dora Korpar, 2015-10-19"\n");
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 
-			return -1;
+	return (1);
+
+}
