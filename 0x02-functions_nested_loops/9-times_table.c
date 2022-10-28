79a95b06e8598a55d200f7eb8d5731c8af546534
attah42 <you@example.comgit config --global user.name Your>
2022-10-28 09:21:01 -0700

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,13 +12,13 @@ void times_table(void)
 
 	for (num = 0; num <= 9; ++num)
 	{
-	_putchar(48);
-	for (mult =0; mult <= 9; ++mult);
+		_putchar(48);
+		for (mult =0; mult <= 9; ++mult);
 	{
-	_putchar(,);
-	_putchar(,);
+		_putchar(',');
+		_putchar(' ');
 
-	prod = num * mult;
+		prod = num * mult;
 	/*
 	 * put space in product
 	 * place the first digit into two numbets
