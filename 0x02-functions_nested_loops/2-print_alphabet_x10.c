3f455a46c4a609c03fbe3e8b2f5753b5550abd12
attah42 <you@example.comgit config --global user.name Your>
2022-10-28 10:33:51 -0700

    newe

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
-#include "main"
+#include "main.h"
 /**
-* print_alphabet_x10-> print the lower case alphabets 
+* print_alphabet_x10-> print the lower case alphabets
 */
 void print_alphabet_x10(void)
 {
 	int j;
 	int x;
-	
+
 	for (x = 0; x < 10; x++)
 	{
-		for (j ='a'; j <= 'z'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(j);
+		_putchar(j);
 		}
 		_putchar('\n');
 	}
